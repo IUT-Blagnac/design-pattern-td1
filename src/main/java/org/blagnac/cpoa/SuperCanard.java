9a5d371dde0b988f718d102e84e8bf626e460a02
Jean-Michel Bruel <jbruel@gmail.com>
2020-10-31 18:35:29 +0100

    init commit

---
+++ src/main/java/org/blagnac/cpoa/SuperCanard.java
@@ -0,0 +1,18 @@
+package org.blagnac.cpoa;
+
+/**
+ * @author bruel
+ * @navassoc - - 1 Canard
+ * @note Main de test simple
+ *
+ */
+public class SuperCanard {
+
+	public static void main(String[] args) {
+		Canard c1 = new Colvert();
+		String info;
+		info = c1.cancaner();
+		info = c1.nager();
+		info = c1.afficher();
+	}
+}
